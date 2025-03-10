4632523f687cde66a6cf45ec3202c57b9285785c
lucasomsousa <lucasomsousa@gmail.com>
2025-03-10 15:32:55 -0300

    Remove unused headers variable

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -2,7 +2,6 @@
 
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
-import { headers } from 'next/headers'
 
 import { createClient } from '@/utils/supabase/server'
 
